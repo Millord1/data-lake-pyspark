77d4467bd0ac59027e31b471e176df63d6bc90bb
oka-ds <ronan.pele1@gmail.com>
2026-08-31 16:18:15 +0200

    modification du code mongo pour standardisation avn avec code theau

--- src/config/database.py
+++ src/config/database.py
@@ -1,5 +1,3 @@
-from enum import StrEnum
-
 KAFKA_TOPIC = "velib-status"
 
 
@@ -17,21 +15,3 @@ class StorageConfig:
         if dataset_name:
             path = f"{path}/{dataset_name}"
         return path
-
-
-class GTFSTableNames(StrEnum):
-    agency: str = "agency"
-    calendar: str = "calendar"
-    calendar_dates: str = "calendar_dates"
-    routes: str = "routes"
-    stop_times: str = "stop_times"
-    stops: str = "stops"
-    transfers: str = "transfers"
-    trips: str = "trips"
-
-
-class SourcesUrls(StrEnum):
-    velib_station: str = "https://opendata.paris.fr/api/explore/v2.1/catalog/datasets/velib-emplacement-des-stations/exports/parquet?lang=fr&timezone=Europe%2FBerlin"
-    velib_historique: str = "https://github.com/lovasoa/historique-velib-opendata/releases/download/latest/stations.zip"
-    velib_api: str = "https://opendata.paris.fr/api/explore/v2.1/catalog/datasets/velib-emplacement-des-stations/records?"
-    meteo: str = ""
